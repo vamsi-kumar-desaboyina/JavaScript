2b9a01bb7c992530769ca87f0df6cf1cca918813
vamsi-kumar-desaboyina <desaboyinavamsi@gmail.com>
2025-11-28 00:55:02 +0530

    dompratice

---
+++ JavaScript/SetIntervel.js
@@ -0,0 +1,17 @@
+// console.log("vamsi1");
+// setInterval(function () {
+//   console.log("vamsi2");
+// }, 1000);
+
+// setInterval(function () {
+//   console.log("vamsi3");
+// }, 1000);
+
+console.log("vamsi1");
+setInterval(function () {
+  console.log("vamsi2");
+}, 2000);
+
+setInterval(function () {
+  console.log("vamsi3");
+}, 1000);
---
+++ JavaScript/SetTimeOut.js
@@ -0,0 +1,29 @@
+// console.log("vamsi1");
+// console.log("vamsi2");
+// console.log("vamsi3");
+
+// console.log("vamsi1");
+// function f1() {
+//   console.log("vamsi2");
+// }
+// setTimeout(f1, 3000);
+// console.log("vamsi3");
+
+// console.log("vamsi1");
+// function f1() {
+//   console.log("vamsi2");
+// }
+// setTimeout(f1, 3000);
+// function f2() {
+//   console.log("vamsi3");
+// }
+// setTimeout(f2, 1000);
+
+console.log("vamsi1");
+setTimeout(function () {
+  console.log("vamsi2");
+}, 1000);
+
+setTimeout(function () {
+  console.log("vamsi3");
+}, 1000);
