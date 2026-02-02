56466535d4315303c9f3ce736899046a742c769a
vamsi-kumar-desaboyina <desaboyinavamsi@gmail.com>
2026-02-02 14:54:55 +0530

    advancedjs

--- JavaScript/pratices/index.js
+++ JavaScript/pratices/index.js
@@ -1,10 +1,19 @@
-console.log("Try programiz.pro");
-setTimeout(function()
-{
-    console.log("dd");
-});
-console.log("Ddsdw");
+// console.log("Try programiz.pro");
+// setTimeout(function()
+// {
+//     console.log("dd");
+// });
+// console.log("Ddsdw");
 
-var a=10;
-console.log(a);
-console.log(window)
+// var a=10;
+// console.log(a);
+// console.log(window)
+
+alert("hello");
+alert("hello");
+alert("hello");
+function f1()
+{
+    alert("hello");
+}
+f1();
